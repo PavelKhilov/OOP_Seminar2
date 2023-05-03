a304b30acf1cf875c7fb207a2ab1b3d6c168deff
Pavel <khilov.pavel@eyandex.ru>
2023-05-03 23:59:06 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/task1/Obstacle.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.task1;
+
+/**
+ * Препятствие
+ */
+public interface Obstacle {
+
+    /**
+     * Длинна припятствия
+     * @return
+     */
+    int getLength();
+
+    /**
+     * Высота препятствия
+     * @return
+     */
+    int getHeight();
+}
---
+++ src/main/java/ru/geekbrains/task1/Runner.java
@@ -0,0 +1,43 @@
+package ru.geekbrains.task1;
+
+/**
+ * Базовый интерфейс - Бегун
+ */
+public interface Runner {
+
+    String getName();
+
+    int getMaxRun();
+
+    int getMaxJump();
+
+    /**
+     * Прыжок
+     * @param height Высота прыжка
+     * @return результат прыжка (удалось перепрыгнуть препятствие или нет)
+     */
+    default boolean jump(int height){
+        if(height <= getMaxJump()){
+            System.out.printf("%s успешно перепрыгнул через стену %d м.\n", getName(), height);
+            return true;
+        } else{
+            System.out.printf("%s не смог перепругнуть через стену %d м. Остался на месте\n", getName(), height);
+            return false;
+        }
+    }
+
+    /**
+     * Бег
+     * @param distance дистанция
+     * @return результат забега (удалось пробежать дистанцию или нет)
+     */
+    default boolean run(int distance){
+        if(distance <= getMaxRun()){
+            System.out.printf("%s успешно пробежал по дорожке %d м.\n", getName(), distance);
+            return true;
+        } else{
+            System.out.printf("%s не смог пробежать по дорожке %d м. Остался на месте\n", getName(), distance);
+            return false;
+        }
+    }
+}
